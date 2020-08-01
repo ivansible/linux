8708e403325dbbcebc1c4182bcfa4f70b0975436
Ivan Andreev <ivandeex@gmail.com>
2020-08-01 16:25:23 +0300

    lin-net: add test hurricane ip6 on loopback

--- roles/lin-net/files/he6-up-ferm.sh
+++ roles/lin-net/files/he6-up-ferm.sh
@@ -1,9 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 #set -x
 set -e
-if [ -n "$IF_HE6_NET48" ] && [ -n "$IF_HE6_HENET" ]; then
+if [[ $IF_HE6_NET48 ]] && [[ $IF_HE6_HENET ]]; then
     ip -6 route replace default from "$IF_ADDRESS"   dev "$IFACE" proto static
     ip -6 route replace default from "$IF_HE6_NET48" dev "$IFACE" proto static
     ip -6 route replace to "$IF_HE6_HENET" dev "$IFACE" proto static
+fi
+if [[ $IF_HE6_LOOPIP ]] && [[ $IF_HE6_NET48 ]]; then
+    ip -6 addr replace "${IF_ADDRESS%%::*}::${IF_HE6_LOOPIP}"   dev lo:2
+    ip -6 addr replace "${IF_HE6_NET48%%::*}::${IF_HE6_LOOPIP}" dev lo:2
 fi
 exit 0
