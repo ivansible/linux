61e4debba6ccb1f1e264222807fc821000c557fd
Ivan Andreev <ivandeex@gmail.com>
2020-08-28 21:31:13 +0300

    lin-keepalived: dynamic local routes

--- roles/lin-keepalived/templates/route-ctl.sh
+++ roles/lin-keepalived/templates/route-ctl.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
+#set -x
+
 state=${1:-}
-name=${2:-}
-iface=${3:-}
+name=${2:-}   # shellcheck disable=SC2034
+iface=${3:-}  # shellcheck disable=SC2034
 target=${4:-}
 vip=${5:-}
 prio=${6:-}
+
 case "$state" in
   up)
     ip route del "$target" via "$vip" prio "$prio"
     ;;
-  down)
+  dn)
     ip route add "$target" via "$vip" prio "$prio"
     ;;
 esac
+
+#echo "$(date) -- $? $state $target $vip $prio" >>/tmp/ping
+exit 0
--- roles/lin-keepalived/templates/route-ping.sh
+++ roles/lin-keepalived/templates/route-ping.sh
@@ -1,3 +1,36 @@
 #!/bin/bash
-shift 1
-fping -a -r1 "$@" |grep -Eq '[.:]'
+#set -x
+
+name=${1:-}  # shellcheck disable=SC2034
+target=${2:-}
+prio=${3:-}
+shift 3
+
+declare -A gateways
+for addr in "$@"; do
+  gateways[$addr]=$(
+    ip -4 -o route get "$addr" |
+    grep -Eo '(via|src) ([^ ]+)' |
+    head -1 |
+    cut -c5-)
+done
+
+retval=1
+lead=$(fping -a -r1 "$@" | head -1)
+if [ -n "$lead" ]; then
+    gw1=${gateways[$lead]}
+    #echo "$(date) radd $target $gw1 $prio" >>/tmp/ping
+    ip -4 route repl "$target" via "$gw1" prio "$prio"
+    retval=0
+fi
+
+for addr in "$@"; do
+  if [ "$addr" != "$lead" ]; then
+    gw=${gateways[$addr]}
+    #echo "$(date) rdel $target $gw $prio" >>/tmp/ping
+    ip -4 route del "$target" via "$gw" prio "$prio" 2>/dev/null
+  fi
+done
+
+#echo "$(date) ping $retval $target $gw1" >>/tmp/ping
+exit $retval
