9309afcd99693e369a0d41c52c3e27d4236d1602
Ivan Andreev <ivandeex@gmail.com>
2020-08-29 04:45:14 +0300

    lin-keepalived: add ipv6 support

--- roles/lin-keepalived/templates/route-ctl.sh
+++ roles/lin-keepalived/templates/route-ctl.sh
@@ -3,17 +3,18 @@
 
 state=${1:-}
 name=${2:-}   # shellcheck disable=SC2034
-iface=${3:-}  # shellcheck disable=SC2034
-target=${4:-}
-vip=${5:-}
-prio=${6:-}
+dev=${3:-}    # shellcheck disable=SC2034
+proto=${4:-}
+target=${5:-}
+vip=${6:-}
+prio=${7:-}
 
 case "$state" in
   up)
-    ip route del "$target" via "$vip" prio "$prio"
+    ip "$proto" route del "$target" via "$vip" prio "$prio"
     ;;
   dn)
-    ip route add "$target" via "$vip" prio "$prio"
+    ip "$proto" route add "$target" via "$vip" prio "$prio"
     ;;
 esac
 
--- roles/lin-keepalived/templates/route-ping.sh
+++ roles/lin-keepalived/templates/route-ping.sh
@@ -2,25 +2,27 @@
 #set -x
 
 name=${1:-}  # shellcheck disable=SC2034
-target=${2:-}
-prio=${3:-}
-shift 3
+proto=${2:-}
+target=${3:-}
+prio=${4:-}
+shift 4
 
 declare -A gateways
 for addr in "$@"; do
   gateways[$addr]=$(
-    ip -4 -o route get "$addr" |
+    ip "$proto" -o route get "$addr" |
     grep -Eo '(via|src) ([^ ]+)' |
     head -1 |
     cut -c5-)
 done
 
 retval=1
-lead=$(fping -a -r1 "$@" | head -1)
+[ "$proto" = '-6' ] && pinger=fping6 || pinger=fping
+lead=$($pinger -a -r1 "$@" | head -1)
 if [ -n "$lead" ]; then
     gw1=${gateways[$lead]}
     #echo "$(date) radd $target $gw1 $prio" >>/tmp/ping
-    ip -4 route repl "$target" via "$gw1" prio "$prio"
+    ip "$proto" route replace "$target" via "$gw1" prio "$prio" 2>/dev/null
     retval=0
 fi
 
@@ -28,7 +30,7 @@ for addr in "$@"; do
   if [ "$addr" != "$lead" ]; then
     gw=${gateways[$addr]}
     #echo "$(date) rdel $target $gw $prio" >>/tmp/ping
-    ip -4 route del "$target" via "$gw" prio "$prio" 2>/dev/null
+    ip "$proto" route del "$target" via "$gw" prio "$prio" 2>/dev/null
   fi
 done
 
