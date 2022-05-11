ae10d421f7907016c57b1979fc7c451eb5b34dba
Ivan Andreev <ivandeex@gmail.com>
2022-05-11 15:03:56 +0300

    net/he6: configure both /64 and /48 subnets

--- roles/net/files/he6-up-ferm.sh
+++ roles/net/files/he6-up-ferm.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 #set -x
 set -e
-if [[ $IF_HE6_NET48 ]] && [[ $IF_HE6_HENET ]]; then
-    ip -6 route replace default from "$IF_ADDRESS"   dev "$IFACE" proto static
-    ip -6 route replace default from "$IF_HE6_NET48" dev "$IFACE" proto static
-    ip -6 route replace to "$IF_HE6_HENET" dev "$IFACE" proto static
+
+[[ $IF_HE6_HENET ]] || exit 0
+
+ip   -6 route replace           to "$IF_HE6_HENET" dev "$IFACE" proto static
+ip   -6 route replace default from "$IF_ADDRESS"   dev "$IFACE" proto static
+if [[ $IF_HE6_NET64 ]]; then
+  ip -6 route replace default from "$IF_HE6_NET64" dev "$IFACE" proto static
 fi
-if [[ $IF_HE6_LOOPIP ]] && [[ $IF_HE6_NET48 ]]; then
-    ip -6 addr replace "${IF_ADDRESS%%::*}::${IF_HE6_LOOPIP}"   dev lo:2
-    ip -6 addr replace "${IF_HE6_NET48%%::*}::${IF_HE6_LOOPIP}" dev lo:2
+if [[ $IF_HE6_NET48 ]]; then
+  ip -6 route replace default from "$IF_HE6_NET48" dev "$IFACE" proto static
 fi
+
+[[ $IF_HE6_LOOPIP ]] || exit 0
+
+ip   -6 addr replace "${IF_ADDRESS%%::*}::${IF_HE6_LOOPIP}"   dev lo:2
+if [[ $IF_HE6_NET64 ]]; then
+  ip -6 addr replace "${IF_HE6_NET64%%::*}::${IF_HE6_LOOPIP}" dev lo:2
+fi
+if [[ $IF_HE6_NET48 ]]; then
+  ip -6 addr replace "${IF_HE6_NET48%%::*}::${IF_HE6_LOOPIP}" dev lo:2
+fi
+
 exit 0
